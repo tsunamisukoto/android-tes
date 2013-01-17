b56cd91db6e5af701edbea7ae03dd33932d46606
tsunamisukoto@gmail.com <tsunamisukoto@hotmail.com>
2013-01-17 10:23:52 +0000

    Renames. fixed a bunch of things like random movement for ai

--- Warlock/src/NPC/EllipseMovingAI.java
+++ Warlock/src/NPC/EllipseMovingAI.java
@@ -25,28 +25,37 @@ public class EllipseMovingAI extends Enemy {
 	
 	public void Update() {
 		 i+=1;
-			angle += 0.005;
+		
 		 if(i%50==49)
 		 {
+			 
+				angle =(float)Math.random()*360;
+			
 		// RenderThread.addObject(new Projectile(position,
 		// RenderThread.archie.position.get(),this));
 	
 		// RenderThread.addObject(new Projectile(position,
 		// RenderThread.gameObjects.get(0).position.get(),this));
 
-		double _x = (RenderThread.l.platform.Size.x / 2 - (RenderThread.l.platform.Size.x / 10))
-				* Math.cos((double) angle % 360)
-				+ RenderThread.l.platform.Position.x;
-		double _y = (RenderThread.l.platform.Size.y / 2 - (RenderThread.l.platform.Size.y / 10))
-				* Math.sin((double) angle % 360)
-				+ RenderThread.l.platform.Position.y;
 
-		feet = new Vector((float) _x, (float) _y);
-		 destination = feet.get();
+		destination =PositiononEllipse(angle);
+	
 		//position = new Vector(feet.x - size.x / 2, feet.y - size.y);
 		// Log.d("Ellipse",destination.x+ " , "+ destination.y);
 		 }
 
 		super.Update();
+	}
+	protected Vector PositiononEllipse(float _angle)
+	{
+		float _x = (RenderThread.l.platform.Size.x / 2 - (RenderThread.l.platform.Size.x / 10))
+				* (float)Math.cos((double) _angle % 360)
+				+ RenderThread.l.platform.Position.x;
+		float _y = (RenderThread.l.platform.Size.y / 2 - (RenderThread.l.platform.Size.y / 10))
+				* (float)Math.sin((double) _angle % 360)
+				+ RenderThread.l.platform.Position.y;
+return new Vector(_x,_y);
+
+	
 	}
 }
--- Warlock/src/com/example/warlockgame/MenuActivity.java
+++ Warlock/src/com/example/warlockgame/MenuActivity.java
@@ -1,5 +1,8 @@
 package com.example.warlockgame;
 
+import Platform.DonutPlatform;
+import Platform.EllipticalPlatform;
+import Tools.Vector;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -26,7 +29,25 @@ public class MenuActivity extends Activity {
         B2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	RenderThread.gameObjects.clear();
-            	RenderThread.l=null;
+            	if(RenderThread.l!=null)
+            	RenderThread.l.platform = new EllipticalPlatform(
+    					new Vector(2800,900), 
+    					new Vector(2500,1250)
+    				);
+           	 Intent myIntent = new Intent(MenuActivity.this, WarlockGame.class);
+           	 MenuActivity.this.startActivity(myIntent);
+            }
+        });
+     	final Button B3 = (Button) findViewById(R.id.button3);
+        B3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	RenderThread.gameObjects.clear();
+            	if(RenderThread.l!=null)
+            	RenderThread.l.platform = new DonutPlatform(
+    					new Vector(2800,900), 
+    					new Vector(2500,1250), 
+    					new Vector(1000,500)
+    				);
            	 Intent myIntent = new Intent(MenuActivity.this, WarlockGame.class);
            	 MenuActivity.this.startActivity(myIntent);
             }
