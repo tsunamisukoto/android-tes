58b4a5a40abe11310700a29d5d0b89097f530c58
tsunamisukoto@gmail.com <tsunamisukoto@hotmail.com>
2013-08-13 14:46:12 +0000

    BETTER game loop. smoothed out things. made draw more efficient

--- WarlocksGradle/MyApplication/build/source/r/debug/com/developmental/myapplication/R.java
+++ WarlocksGradle/MyApplication/build/source/r/debug/com/developmental/myapplication/R.java
@@ -197,39 +197,40 @@ containing a value of this type.
         public static final int tileset1=0x7f020026;
     }
     public static final class id {
-        public static final int PasswordInput=0x7f060013;
-        public static final int UsernameInput=0x7f060011;
+        public static final int PasswordInput=0x7f060014;
+        public static final int UsernameInput=0x7f060012;
         public static final int button=0x7f06000e;
         public static final int button1=0x7f060009;
         public static final int button2=0x7f06000a;
         public static final int button3=0x7f06000c;
         public static final int button4=0x7f06000d;
-        public static final int changetomenu=0x7f060014;
-        public static final int createroom=0x7f060017;
-        public static final int debug=0x7f060019;
+        public static final int button5=0x7f060010;
+        public static final int changetomenu=0x7f060015;
+        public static final int createroom=0x7f060018;
+        public static final int debug=0x7f06001a;
         public static final int dummy_button=0x7f060007;
-        public static final int editText=0x7f06001b;
+        public static final int editText=0x7f06001c;
         public static final int fullscreen_content=0x7f060005;
         public static final int fullscreen_content_controls=0x7f060006;
-        public static final int goglesignin=0x7f060015;
+        public static final int goglesignin=0x7f060016;
         public static final int hybrid=0x7f060004;
         public static final int imageView=0x7f060008;
-        public static final int lefthandmode=0x7f06001e;
-        public static final int listView=0x7f060010;
+        public static final int lefthandmode=0x7f06001f;
+        public static final int listView=0x7f060011;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
         public static final int progressBar2=0x7f06000b;
-        public static final int quickmatch=0x7f060016;
-        public static final int radioButton=0x7f06001a;
-        public static final int radioButton2=0x7f06001d;
-        public static final int radioButton3=0x7f06001c;
-        public static final int radioOptions=0x7f060018;
+        public static final int quickmatch=0x7f060017;
+        public static final int radioButton=0x7f06001b;
+        public static final int radioButton2=0x7f06001e;
+        public static final int radioButton3=0x7f06001d;
+        public static final int radioOptions=0x7f060019;
         public static final int satellite=0x7f060002;
-        public static final int singleplayerbeginbutton=0x7f060020;
+        public static final int singleplayerbeginbutton=0x7f060021;
         public static final int terrain=0x7f060003;
         public static final int textView=0x7f06000f;
-        public static final int textView2=0x7f060012;
-        public static final int textView3=0x7f06001f;
+        public static final int textView2=0x7f060013;
+        public static final int textView3=0x7f060020;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
@@ -238,7 +239,8 @@ containing a value of this type.
         public static final int game_lobby=0x7f030003;
         public static final int login_layout=0x7f030004;
         public static final int multiplayeroptions=0x7f030005;
-        public static final int singleplayeroption=0x7f030006;
+        public static final int shop=0x7f030006;
+        public static final int singleplayeroption=0x7f030007;
     }
     public static final class menu {
         public static final int optionsmenu=0x7f090000;
--- WarlocksGradle/MyApplication/src/main/java/com/developmental/myapplication/GameThread.java
+++ WarlocksGradle/MyApplication/src/main/java/com/developmental/myapplication/GameThread.java
@@ -29,7 +29,7 @@ public class GameThread extends Thread {
    public static int Gamestep = 0;
    public static ArrayList<Finger> fingers =new ArrayList<Finger>();
     private static final String TAG = GameThread.class.getSimpleName();
-    static final long FPS = 15;
+    static final long FPS = 20;
     // Surface holder that can access the physical surface
     private final SurfaceHolder surfaceHolder;
     // The actual view that handles inputs
@@ -73,7 +73,7 @@ Gamestep+=1;
           //  if(i++%3 == 0)
         if(RenderThread.finger!=null)
                 if(RenderThread.finger.down)
-                RenderThread.c.getGamesClient().sendUnreliableRealTimeMessageToAll(Serializer.SerializetoBytes(new NetworkFinger(Gamestep,RenderThread.finger.WorldPositions(),Global.playerno,selectedSpell)), RenderThread.c.mRoom.getRoomId());
+                RenderThread.c.getGamesClient().sendUnreliableRealTimeMessageToAll(Serializer.SerializetoBytes(new NetworkFinger(Gamestep+3,RenderThread.finger.WorldPositions(),Global.playerno,selectedSpell)), RenderThread.c.mRoom.getRoomId());
         for(int f = 0; f<RenderThread.popupTexts.size();f++)
         {
             RenderThread.popupTexts.get(f).Update();
@@ -86,7 +86,6 @@ Gamestep+=1;
         Collections.sort(RenderThread.gameObjects);
 
     }
-
     public static int s = 0;
 
     public void Collision() {
@@ -103,7 +102,7 @@ Gamestep+=1;
 
         for (int x = 0; x < RenderThread.gameObjects.size(); x++) {
             GameObject g = RenderThread.gameObjects.get(x);
-            for (int y = 0; y < RenderThread.gameObjects.size(); y++) {
+            for (int y = x+1; y < RenderThread.gameObjects.size(); y++) {
                 if (RenderThread.gameObjects.size() > y
                         && RenderThread.gameObjects.size() > x)
                     if (y != x)
@@ -140,50 +139,142 @@ public static GamesClient gamesClient;
     public static Room room;
     public static Quadtree q = new Quadtree(0, new RectF(0, 0, Global.WORLD_BOUND_SIZE.x, Global.WORLD_BOUND_SIZE.y));
 int i = 0;
+
+
+
+
+    private boolean paused = false;
+    private int fps = 30;
+    private int frameCount = 0;
     @Override
-    public void run() {
-        long ticksPS = 1000 / FPS;
-        long startTime;
-        long sleepTime;
-        Canvas canvas;
-       // gamesClient.connect();
-        Log.d("INET", "GAME THREAD STARTED");
-        while (running) {
-            canvas = null;
-            startTime = System.currentTimeMillis();
-            // try locki        n   g   th  e       c   an  vas for exclusive pixel editing
-            // in the surface
-            try {
+    public void run()
+    {
+        Canvas canvas=null;
+        //This value would probably be stored elsewhere.
+        final double GAME_HERTZ = 30.0;
+        //Calculate how many ns each frame should take for our target game hertz.
+        final double TIME_BETWEEN_UPDATES = 1000000000 / GAME_HERTZ;
+        //At the very most we will update the game this many times before a new render.
+        //If you're worried about visual hitches more than perfect timing, set this to 1.
+        final int MAX_UPDATES_BEFORE_RENDER = 5;
+        //We will need the last update time.
+        double lastUpdateTime = System.nanoTime();
+        //Store the last time we rendered.
+        double lastRenderTime = System.nanoTime();
 
-                canvas = this.surfaceHolder.lockCanvas();
-                synchronized (this.surfaceHolder) {
+        //If we are able to get as high as this FPS, don't render again.
+        final double TARGET_FPS = 60;
+        final double TARGET_TIME_BETWEEN_RENDERS = 1000000000 / TARGET_FPS;
+
+        //Simple way of finding FPS.
+        int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+
+        while (running)
+        {
+            double now = System.nanoTime();
+            int updateCount = 0;
+
+            if (!paused)
+            {
+                try {
+                    canvas = this.surfaceHolder.lockCanvas();
+
+                //Do as many game updates as we need to, potentially playing catchup.
+                while( now - lastUpdateTime > TIME_BETWEEN_UPDATES && updateCount < MAX_UPDATES_BEFORE_RENDER )
+                {
                     Update();
-                   //     Log.d("INET",(String)ServerThread.getLocalIpAddress());
-                    // update game state
-                    // render state to the screen
-                    // draws the canvas on the panel
-                  //  if(Global.Multiplayer)
-                     //   if(RenderThread.finger!=null)gamesClient.sendUnreliableRealTimeMessageToAll(Serializer.SerializetoBytes(RenderThread.finger),room.getRoomId());
-                    if(canvas!=null)
-                    this.renderThread.onDraw(canvas);
+                    lastUpdateTime += TIME_BETWEEN_UPDATES;
+                    updateCount++;
                 }
-            } finally {
-                // in case of an exception the surface is not left in
-                // an inconsistent state
-                if (canvas != null)
-                    this.surfaceHolder.unlockCanvasAndPost(canvas);
-            } // end finally
 
-            sleepTime = ticksPS - (System.currentTimeMillis() - startTime);
-            try {
-                if (sleepTime > 0)
-                    sleep(sleepTime);
-                else
-                    sleep(10);
-            } catch (Exception e) {
+                //If for some reason an update takes forever, we don't want to do an insane number of catchups.
+                //If you were doing some sort of game that needed to keep EXACT time, you would get rid of this.
+                if ( now - lastUpdateTime > TIME_BETWEEN_UPDATES)
+                {
+                    lastUpdateTime = now - TIME_BETWEEN_UPDATES;
+                }
+
+                //Render. To do so, we need to calculate interpolation for a smooth render.
+                float interpolation = Math.min(1.0f, (float) ((now - lastUpdateTime) / TIME_BETWEEN_UPDATES) );
+                renderThread.onDraw(canvas);
+                lastRenderTime = now;
+                }
+                finally {
+                    if (canvas != null)
+                        this.surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+                //Update the frames we got.
+                int thisSecond = (int) (lastUpdateTime / 1000000000);
+                if (thisSecond > lastSecondTime)
+                {
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    fps = frameCount;
+                    frameCount = 0;
+                    lastSecondTime = thisSecond;
+                }
+
+                //Yield until it has been at least the target time between renders. This saves the CPU from hogging.
+                while ( now - lastRenderTime < TARGET_TIME_BETWEEN_RENDERS && now - lastUpdateTime < TIME_BETWEEN_UPDATES)
+                {
+                    Thread.yield();
+
+                    //This stops the app from consuming all your CPU. It makes this slightly less accurate, but is worth it.
+                    //You can remove this line and it will still work (better), your CPU just climbs on certain OSes.
+                    //FYI on some OS's this can cause pretty bad stuttering. Scroll down and have a look at different peoples' solutions to this.
+                    try {Thread.sleep(1);} catch(Exception e) {}
+
+                    now = System.nanoTime();
+                }
             }
         }
-        Log.d("INET","CLOSED GAME THREAD");
     }
+
+
+//
+//    @Override
+//    public void run() {
+//        long ticksPS = 1000 / FPS;
+//        long startTime;
+//        long sleepTime;
+//        Canvas canvas;
+//       // gamesClient.connect();
+//        Log.d("INET", "GAME THREAD STARTED");
+//        while (running) {
+//            canvas = null;
+//            startTime = System.currentTimeMillis();
+//            // try locki        n   g   th  e       c   an  vas for exclusive pixel editing
+//            // in the surface
+//            try {
+//
+//                canvas = this.surfaceHolder.lockCanvas();
+//                synchronized (this.surfaceHolder) {
+//                    Update();
+//                   //     Log.d("INET",(String)ServerThread.getLocalIpAddress());
+//                    // update game state
+//                    // render state to the screen
+//                    // draws the canvas on the panel
+//                  //  if(Global.Multiplayer)
+//                     //   if(RenderThread.finger!=null)gamesClient.sendUnreliableRealTimeMessageToAll(Serializer.SerializetoBytes(RenderThread.finger),room.getRoomId());
+//                    if(canvas!=null)
+//                    this.renderThread.onDraw(canvas);
+//                }
+//            } finally {
+//                // in case of an exception the surface is not left in
+//                // an inconsistent state
+//                if (canvas != null)
+//                    this.surfaceHolder.unlockCanvasAndPost(canvas);
+//            } // end finally
+//
+//            sleepTime = ticksPS - (System.currentTimeMillis() - startTime);
+//            try {
+//                if (sleepTime > 0)
+//                    sleep(sleepTime);
+//                else
+//                    sleep(10);
+//            } catch (Exception e) {
+//            }
+//        }
+//        Log.d("INET","CLOSED GAME THREAD");
+//    }
 
 }
