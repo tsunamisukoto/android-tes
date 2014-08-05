07f69bd2825ed7e250cf39ccd5c2540923998c22
tsunamisukoto@gmail.com <tsunamisukoto@gmail.com@614310a4-7546-7f03-1f60-2391b538efb4>
2014-08-05 08:00:34 +0000

    Test Change with Android Studio

--- trunk/Warlocks(Working Copy)/app/src/main/java/Game/DamageType.java
+++ trunk/Warlocks(Working Copy)/app/src/main/java/Game/DamageType.java
@@ -2,5 +2,5 @@ package Game;
 
 //this is the enum for what TYPE of object the gameobject is, to differentiate between how other objects should respond to it
 public enum DamageType {
-    Spell, Lava,hi
+    Spell, Lava
 }
