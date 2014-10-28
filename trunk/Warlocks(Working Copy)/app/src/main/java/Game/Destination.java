f12318f639d59c1b46596d395f540ff628800c99
tsunamisukoto@gmail.com <tsunamisukoto@gmail.com@614310a4-7546-7f03-1f60-2391b538efb4>
2014-10-28 08:49:34 +0000

    Added in a placeholder for Destination marker

--- trunk/Warlocks(Working Copy)/app/src/main/java/Game/Destination.java
+++ trunk/Warlocks(Working Copy)/app/src/main/java/Game/Destination.java
@@ -4,40 +4,37 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
+import com.developmental.warlocks.R;
+
 import Tools.Vector;
+import developmental.warlocks.GL.NewHeirarchy.Renderable;
+import developmental.warlocks.Global;
 
 /**
  * Class for Storing and drawing a destination vector, like when the player is tapping around the map
  * Created by Scott on 5/30/13.
  */
-public class Destination {
+public class Destination extends Renderable {
 
-    protected float x;
-    protected float y;
-    protected int Frame = 0;
+
+
     protected Paint paint = new Paint();
 
-    protected Paint paint2 = new Paint();
 
-    public Destination(Vector d) {
-        x = d.x;
-        y = d.y;
-        paint.setColor(Color.GREEN);
-        paint.setStrokeWidth(3);
-        paint2.setColor(Color.BLACK);
-        paint2.setStrokeWidth(14);
 
+    public Destination(Vector v) {
+        super(R.drawable.effect_particle);
+        this.position= v.get();
+        this.mGrid= Global.spellSpritesMeteor;
     }
 
-    public void Draw(Canvas canvas, float playerx, float playery) {
-        if (Frame < 25) {
-            Frame += 1;
-        }
-        canvas.drawLine(x - Frame - playerx, y - Frame - playery, x + Frame - playerx, y + Frame - playery, paint2);
-        canvas.drawLine(x - Frame - playerx, y + Frame - playery, x + Frame - playerx, y - Frame - playery, paint2);
-        canvas.drawLine(x - Frame - playerx, y - Frame - playery, x + Frame - playerx, y + Frame - playery, paint);
-        canvas.drawLine(x - Frame - playerx, y + Frame - playery, x + Frame - playerx, y - Frame - playery, paint);
-
+    @Override
+    public void Update() {
+        super.Update();
     }
+
+
+
+
 
 }
